ff64f3329cc9732e67952137b2acd7f7c80228f5
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-03-04 11:58:36 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -1,37 +1,37 @@
 import math
-from pyrogram.types import InlineKeyboardButton
 from SHUKLAMUSIC import app
 import config
 from SHUKLAMUSIC.utils.formatters import time_to_seconds
 
+# 🔥 HACK: Pyrogram bypass ke liye raw JSON dictionaries
+def api_btn(text, callback_data=None, url=None, style=None):
+    btn = {"text": text}
+    if callback_data:
+        btn["callback_data"] = callback_data
+    if url:
+        btn["url"] = url
+    if style:
+        btn["style"] = style  # 'primary' = Blue, 'danger' = Red
+    return btn
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
-            ),
+            api_btn(text=_["P_B_1"], callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}", style="primary"),
+            api_btn(text=_["P_B_2"], callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}", style="primary"),
         ],
         [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            )
+            api_btn(text=_["CLOSE_BUTTON"], callback_data=f"forceclose {videoid}|{user_id}", style="danger")
         ],
     ]
     return buttons
-
 
 def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
+    
     if 0 < umm <= 10:
         bar = "▰▱▱▱▱▱▱▱▱▱"
     elif 10 < umm < 20:
@@ -52,111 +52,78 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "▰▰▰▰▰▰▰▰▰▱" 
     else:
         bar = "▰▰▰▰▰▰▰▰▰▰"
-    buttons = [
-                [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=" ᴜᴘᴅᴀᴛᴇ ", url="https://t.me/heartstealer_x",),
-            InlineKeyboardButton(text=" sᴜᴘᴘᴏʀᴛ ", url="https://t.me/+N08m5L1mCTU2NTE1",),
-        ],
         
+    buttons = [
         [
-            
-            InlineKeyboardButton(text=" ˹ ᴍɪᴍɪ ᴛᴜɴᴇs ˼ ♪ ", url="https://t.me/SPSUPPORTT1")
+            # Blue Timer Bar
+            api_btn(text=f"{played} {bar} {dur}", callback_data="GetTimer", style="primary")
         ],
-
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            # Update Blue, Support Red
+            api_btn(text=" ᴜᴘᴅᴀᴛᴇ ", url="https://t.me/heartstealer_x", style="primary"),
+            api_btn(text=" sᴜᴘᴘᴏʀᴛ ", url="https://t.me/+N08m5L1mCTU2NTE1", style="danger"),
+        ],
+        [
+            # Mimi Tunes Blue
+            api_btn(text=" ˹ ᴍɪᴍɪ ᴛᴜɴᴇs ˼ ♪ ", url="https://t.me/SPSUPPORTT1", style="primary")
+        ],
+        [
+            # Close Red
+            api_btn(text=_["CLOSE_BUTTON"], callback_data="close", style="danger")
+        ],
     ]
     return buttons
-
 
 def stream_markup(_, chat_id):
     buttons = [
-
-        
         [
-            InlineKeyboardButton(text=" ᴜᴘᴅᴀᴛᴇ ", url="https://t.me/drxgiveway",),
-            InlineKeyboardButton(text=" sᴜᴘᴘᴏʀᴛ ", url="https://t.me/drx_supportchat",),
+            api_btn(text=" ᴜᴘᴅᴀᴛᴇ ", url="https://t.me/drxgiveway", style="primary"),
+            api_btn(text=" sᴜᴘᴘᴏʀᴛ ", url="https://t.me/drx_supportchat", style="danger"),
         ],
         [
-                   InlineKeyboardButton(text=" ˹ ᴛιᴅᴀʟ ᴛᴜɴᴇs ˼ ♪ ", url="http://t.me/TidalXMusicBot/tidaltunes")
+            api_btn(text=" ˹ ᴛιᴅᴀʟ ᴛᴜɴᴇs ˼ ♪ ", url="http://t.me/TidalXMusicBot/tidaltunes", style="primary")
         ], 
-       
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            api_btn(text=_["CLOSE_BUTTON"], callback_data="close", style="danger")
+        ],
     ]
     return buttons
-
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["P_B_1"],
-                callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
-            ),
+            api_btn(text=_["P_B_1"], callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}", style="primary"),
+            api_btn(text=_["P_B_2"], callback_data=f"SHUKLAPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}", style="primary"),
         ],
         [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
+            api_btn(text=_["CLOSE_BUTTON"], callback_data=f"forceclose {videoid}|{user_id}", style="danger"),
         ],
     ]
     return buttons
-
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["P_B_3"],
-                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
-            ),
+            api_btn(text=_["P_B_3"], callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}", style="primary"),
         ],
         [
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
+            api_btn(text=_["CLOSE_BUTTON"], callback_data=f"forceclose {videoid}|{user_id}", style="danger"),
         ],
     ]
     return buttons
-
 
 def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     query = f"{query[:20]}"
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
-            ),
+            api_btn(text=_["P_B_1"], callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}", style="primary"),
+            api_btn(text=_["P_B_2"], callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}", style="primary"),
         ],
         [
-            InlineKeyboardButton(
-                text="◁",
-                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {query}|{user_id}",
-            ),
-            InlineKeyboardButton(
-                text="▷",
-                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
-            ),
+            api_btn(text="◁", callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}", style="primary"),
+            api_btn(text=_["CLOSE_BUTTON"], callback_data=f"forceclose {query}|{user_id}", style="danger"),
+            api_btn(text="▷", callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}", style="primary"),
         ],
     ]
     return buttons
+    
