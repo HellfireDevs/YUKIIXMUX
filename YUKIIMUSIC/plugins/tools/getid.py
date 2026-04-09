7d60281f8cbc8c21709ea5b37f48e0377b7ffb92
HellfireDevs <devshellfire@gmail.com>
2026-04-09 14:51:36 +0530

    Implement get_video_id function in getid.py
    Add a function to get video file ID from messages.

---
+++ YUKIIMUSIC/plugins/tools/getid.py
@@ -0,0 +1,9 @@
+from pyrogram import filters
+from YUKIIMUSIC import app
+from YUKIIMUSIC.misc import SUDOERS
+
+@app.on_message(filters.video & filters.private & filters.user(SUDOERS))
+async def get_video_id(client, message):
+    # Jaise hi tu video bhejega, bot uski file_id reply kar dega
+    file_id = message.video.file_id
+    await message.reply_text(f"**Yᴇ ʀᴀʜɪ Tᴇʀɪ Fɪʟᴇ ID:**\n\n`{file_id}`")
